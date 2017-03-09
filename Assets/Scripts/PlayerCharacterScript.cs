8310128f84fb8c2a1d62588053d39a8e640a9eb7
Unknown <patrickmonville@hotmail.com>
2017-03-09 15:03:32 +0100

    stuff

--- Assets/Scripts/PlayerCharacterScript.cs
+++ Assets/Scripts/PlayerCharacterScript.cs
@@ -104,7 +104,7 @@ public class PlayerCharacterScript : MonoBehaviour
             GetComponent<AudioSource>().Play();
             _isDead = true;
 
-            Invoke("ReloadScene", 0.5f);
+            Invoke("ReloadScene", 0.2f);
             return;
         }
 
